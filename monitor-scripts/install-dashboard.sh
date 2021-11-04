519864b6949e54a33f1a7265882f5d08ce219a98
briffy <40440683+briffy@users.noreply.github.com>
2021-11-04 23:39:29 +0000

    Delete monitor-scripts directory

--- monitor-scripts/install-dashboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-systemctl stop apache2.service
-systemctl disable apache2.service
-
-systemctl start nginx
-systemctl enable nginx
--- monitor-scripts/wifi-check.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1 > /var/dashboard/statuses/wifi
