5a1aab553aa1bfec107e898a162b40087f00e6cb
briffy <40440683+briffy@users.noreply.github.com>
2021-11-18 11:27:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if id -nG admin | grep -qw "sudo"; then
     cd dashboardinstall
     systemctl stop pm2-pi.service
     systemctl disable pm2-pi.service
-
+    apt-get update
     apt-get --assume-yes install nginx php-fpm php7.3-fpm
 
     mkdir /var/dashboard
