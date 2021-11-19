a537872e816c0e8e3af43169d0c251bac9f54ae1
briffy <40440683+briffy@users.noreply.github.com>
2021-11-19 14:54:35 +0000

    Update dashboard-update.sh

--- monitor-scripts/dashboard-update.sh
+++ monitor-scripts/dashboard-update.sh
@@ -3,31 +3,6 @@ service=$(cat /var/dashboard/services/dashboard-update | tr -d '\n')
 
 if [[ $service == 'start' ]]; then
   echo 'running' > /var/dashboard/services/dashboard-update
-  echo 'Downloading latest release...' > /var/dashboard/logs/dashboard-update.log
-  cd /tmp
-  wget https://raw.githubusercontent.com/briffy/PiscesQoLDashboard/main/latest.tar.gz
-  echo 'Extracting contents...' >> /var/dashboard/logs/dashboard-update.log
-  if test -f latest.tar.gz; then
-    tar -xzf latest.tar.gz
-    cd dashboardinstall
-    cp -r dashboard/* /var/dashboard/
-    cp monitor-scripts/* /etc/monitor-scripts/
-    cp systemd/* /etc/systemd/system/
-    chmod 755 /etc/monitor-scripts/*
-    chown root:www-data /var/dashboard/services/*
-    chown root:www-data /var/dashboard/statuses/*
-    chmod 775 /var/dashboard/services/*
-    chmod 775 /var/dashboard/statuses/*
-    cd systemd
-    echo 'Starting and enabling services...' >> /var/dashboard/logs/dashboard-update.log
-    FILES="*.timer"
-    for f in $FILES;
-    do
-      name=$(echo $f | sed 's/.timer//')
-      systemctl daemon-reload >> /var/dashboard/logs/dashboard-update.log
-      systemctl start $name.timer >> /var/dashboard/logs/dashboard-update.log
-      systemctl enable $name.timer >> /var/dashboard/logs/dashboard-update.log
-      systemctl start $name.service >> /var/dashboard/logs/dashboard-update.log
-    done
+  wget https://raw.githubusercontent.com/briffy/PiscesQoLDashboard/main/update.sh -O - | sudo bash
   fi
 fi
