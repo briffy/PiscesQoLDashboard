d750313dc200538bbeae1733fc55c864d1ab5857
briffy <40440683+briffy@users.noreply.github.com>
2021-11-19 14:57:03 +0000

    Update dashboard-update.sh

--- monitor-scripts/dashboard-update.sh
+++ monitor-scripts/dashboard-update.sh
@@ -4,5 +4,4 @@ service=$(cat /var/dashboard/services/dashboard-update | tr -d '\n')
 if [[ $service == 'start' ]]; then
   echo 'running' > /var/dashboard/services/dashboard-update
   wget https://raw.githubusercontent.com/briffy/PiscesQoLDashboard/main/update.sh -O - | sudo bash
-  fi
 fi
