efbbd3ec4b30e86541191fb16a5d7017dfe75d35
iotzo <diado.iotzo@gmail.com>
2021-11-19 08:31:48 +0200

    curl -s 'https://api.helium.io/v1/blocks/height'
    <?xml version="1.0" encoding="utf-8"?>
    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
    <html>
      <head>
        <title>429 Slow down gun powder!</title>
      </head>
      <body>
        <h1>Error 429 Slow down gun powder!</h1>
        <p>Slow down gun powder!</p>
        <h3>Guru Mediation:</h3>
        <p>Details: cache-fra19166-FRA 1637303554 2403683208</p>
        <hr>
        <p>Varnish cache server</p>
      </body>
    </html>
    
    wget -qO- 'https://api.helium.io/v1/blocks/height'
    {"data":{"height":1103346}}

--- monitor-scripts/helium-statuses.sh
+++ monitor-scripts/helium-statuses.sh
@@ -8,8 +8,7 @@ recent_activity_uri="$uri$activity_uri"
 data=$(curl -s $uri)
 recent_activity=$(curl -s $recent_activity_uri)
 echo $recent_activity_uri
-
-height=$(curl -s 'https://api.helium.io/v1/blocks/height' | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
+height=$(wget -qO- 'https://api.helium.io/v1/blocks/height' | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
 online_status=$(echo $data | grep -Po '"online":".*?[^\\]"' | sed -e 's/^"online"://' | tr -d '"')
 lat=$(echo $data | grep -Po '"lat":[^\,]+' | sed -e 's/^"lat"://')
 lng=$(echo $data | grep -Po '"lng":[^\,]+' | sed -e 's/^"lng"://')
