4b2ebe5fb3816039c626176efb47432ca13dbde5
briffy <40440683+briffy@users.noreply.github.com>
2021-11-20 21:41:51 +0000

    Delete dashboard directory

--- dashboard/public/FastSync.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$file = fopen('/var/dashboard/statuses/fastsync', 'w');
-	fwrite($file, "true\n");
-?>
--- dashboard/public/RebootDevice.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$file = fopen('/var/dashboard/statuses/reboot', 'w');
-	fwrite($file, "true\n");
-?>
