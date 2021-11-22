fa86787beca794811b9d086e72d3b468d2d7b46f
Joan Marc Riera Duocastella <mriera@ebi.ac.uk>
2021-11-22 22:34:23 +0000

    updated to briffy

--- monitor-scripts/helium-statuses.sh
+++ monitor-scripts/helium-statuses.sh
@@ -5,11 +5,11 @@ activity_uri="/activity"
 uri="$root_uri$pubkey"
 recent_activity_uri="$uri$activity_uri"
 
-data=$(curl -s $uri)
+data=$(wget -qO- $uri)
 recent_activity=$(curl -s $recent_activity_uri)
 echo $recent_activity_uri
 
-height=$(curl -s 'https://api.helium.io/v1/blocks/height' | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
+height=$(wget -qO- 'https://api.helium.io/v1/blocks/height' | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
 online_status=$(echo $data | grep -Po '"online":".*?[^\\]"' | sed -e 's/^"online"://' | tr -d '"')
 lat=$(echo $data | grep -Po '"lat":[^\,]+' | sed -e 's/^"lat"://')
 lng=$(echo $data | grep -Po '"lng":[^\,]+' | sed -e 's/^"lng"://')
