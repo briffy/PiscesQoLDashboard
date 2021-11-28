9831a48d08a2ed84fa1c4f7d17e530d3883fe284
briffy <40440683+briffy@users.noreply.github.com>
2021-11-28 16:04:43 +0000

    Delete dashboard/public directory

--- dashboard/public/ClearBlockChain.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_POST['confirm'])
-{
-	exec("sudo docker stop miner");
-	exec("sudo rm -rf /home/pi/hnt/miner/blockchain.db/*");
-	exec("sudo rm -rf /home/pi/hnt/miner/ledger.db/*");
-	exec("sudo docker start miner");
-	echo 'Blockchain data cleared.';
-}
