cc77f7ddd94f09df4c89f5d858ea2296ca392eb3
briffy <40440683+briffy@users.noreply.github.com>
2021-11-28 16:06:41 +0000

    Add files via upload

---
+++ dashboard/pages/tools.php
@@ -0,0 +1,58 @@
+<h1>Pisces P100 Outdoor Miner Dashboard - Tools</h1>
+
+<div id="tools_buttons">
+	<ul>
+		<?php
+		$fastsync = trim(shell_exec("cat /var/dashboard/services/fastsync"));
+		if($fastsync == 'stopped')
+		{
+			echo '<li id="fast_sync_button">';
+			echo '<a href="#" onclick="EnableService(\'FastSync\');" title="Fast Sync">';
+		}
+		else
+		{
+			echo '<li id="fast_sync_button_disabled">';
+			echo '<a href="#" title="Fast Sync in progress">';
+		}
+		?>
+		<span class="icon-loop2"></span>
+		<span class="button_title">Fast Sync</span>
+		</a>
+		</li>
+
+		<li id="password_reset_button">
+			<a href="#" onclick="ResetPasswordPrompt();" title="Reset Password">
+				<span class="icon-key"></span>
+				<span class="button_title">Reset Password</span>
+			</a>
+		</li>
+
+		<li id="set_wireless_button">
+			<a href="#" onclick="SetWirelessPrompt();" title="Set WiFi">
+				<span class="icon-wifi"></span>
+				<span class="button_title">Set WiFi</span>
+			</a>
+		</li>
+
+		<li id="update_dashboard_button">
+			<a href="/index.php?page=updatedashboard" title="Update Dashboard">
+				<span class="icon-system_update_tv"></span>
+				<span class="button_title">Update Dashboard</span>
+			</a>
+		</li>
+
+		<li id="update_miner_button">
+			<a href="/index.php?page=updateminer" title="Update Miner">
+				<span class="icon-cloud-check"></span>
+				<span class="button_title">Update Miner</span>
+			</a>
+		</li>
+
+		<li id="clear_blockchain_button">
+			<a href="#" onclick="ClearBlockChainPrompt();" title="Clear BlockChain Data">
+				<span class="icon-warning"></span>
+				<span class="button_title">Clear BlockChain Data</span>
+			</a>
+		</li>
+	</ul>
+</div>
