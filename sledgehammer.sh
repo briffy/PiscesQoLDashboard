99ca0483bcc843df267ddf1ced280cb03e56900e
briffy <40440683+briffy@users.noreply.github.com>
2021-12-05 20:55:31 +0000

    Delete sledgehammer.sh

--- sledgehammer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get install dnsutils --assume-yes
-server=$(nslookup seed.helium.io | grep "Address: " | sed "s/Address: //")
-
-for s in $server; do
-  sudo docker exec miner miner peer connect /ip4/$s/tcp/2154
-done
